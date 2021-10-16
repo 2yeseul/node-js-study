26c00c0e5c1ea84f526e8e9baa362c77ac6c5abe
이예슬 <20152917@sungshin.ac.kr>
2021-10-16 15:31:32 +0900

    express 실습 1

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+// 라우터 모듈인 main.js 를 불러와 app에 전달
+var router = require('./router/main')(app);
+
+// 서버가 읽을 수 있도록 HTML 위치를 정의 
+app.set('views', __dirname + '/views');
+// 서버가 html 렌더링을 할 때 EJS 엔진을 사용하도록 설정
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+var server = app.listen(3000, function () {
+    console.log('Express Server has started on port 3000');
+})
+
+app.use(express.static('public'));
